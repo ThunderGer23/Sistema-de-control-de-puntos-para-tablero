32eb4c68cb4fb739701ccd82ec37b19ced87b4a5
ThunderGer <Jaames11@hotmail.com>
2022-09-01 00:47:42 -0500

    Letra

--- Calculadora/Calculadora.ino
+++ Calculadora/Calculadora.ino
@@ -31,16 +31,133 @@ y posterior traslado a un Arduino Pro Micro:
 */
 
 // include the library code:
-#include <LiquidCrystal.h>
-#include <SoftwareSerial.h>
+// make some custom characters:
+byte heart[8] = {
+  0b00000,
+  0b11000,
+  0b01100,
+  0b00011,
+  0b00011,
+  0b00011,
+  0b00011,
+  0b00111
+};
 
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
+byte smiley[8] = {
+  0b00000,
+  0b01100,
+  0b11000,
+  0b00000,
+  0b00110,
+  0b00010,
+  0b00010,
+  0b10001
+};
 
-void setup(){
+byte frownie[8] = {
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00011,
+  0b11000,
+  0b10000,
+  0b10000,
+  0b11000
+};
+
+byte armsDown[8] = {
+  0b00000,
+  0b01100,
+  0b10010,
+  0b10000,
+  0b10000,
+  0b10110,
+  0b10010,
+  0b01100
+};
+
+byte armsUp[8] = {
+  0b00000,
+  0b00000,
+  0b10000,
+  0b00011,
+  0b11000,
+  0b10000,
+  0b10000,
+  0b10000
+};
+
+byte letter[8] = {
+  0b00000,
+  0b01100,
+  0b10010,
+  0b10010,
+  0b10010,
+  0b10010,
+  0b10010,
+  0b01100
+};
+
+byte lettera[8] = {
+  0b00000,
+  0b11000,
+  0b01000,
+  0b01000,
+  0b01100,
+  0b01010,
+  0b01010,
+  0b11011
+};
+
+byte letterb[8] = {
+  0b11111,
+  0b01110,
+  0b01110,
+  0b00100,
+  0b00100,
+  0b00000,
+  0b01110,
+  0b01110
+};
+
+void setup() {
+  // initialize LCD and set up the number of columns and rows:
+  lcd.begin(16, 2);
+
+  // create a new character
+  lcd.createChar(0, heart);
+  // create a new character
+  lcd.createChar(1, smiley);
+  // create a new character
+  lcd.createChar(2, frownie);
+  // create a new character
+  lcd.createChar(3, armsDown);
+  // create a new character
+  lcd.createChar(4, armsUp);
+  // create a new character
+  lcd.createChar(5, letter);
+  // create a new character
+  lcd.createChar(6, lettera);
+  // create a new character
+  lcd.createChar(7, letterb);
+
+  // set the cursor to the top left
+  lcd.setCursor(0, 0);
+
+  // Print a message to the lcd.
+  //lcd.print("I ");
+  lcd.write(byte(0)); // when calling lcd.write() '0' must be cast as a byte
+  //lcd.print(" Arduino! ");
+  lcd.write((byte)1);
+  lcd.write((byte)2);
+  lcd.write((byte)3);
+  lcd.write((byte)4);
+  lcd.write((byte)5);
+  lcd.write((byte)6);
+  lcd.write((byte)7);
   Serial.begin(9600);
-  LCD.begin(16, 2);
 }
+
 
 
 void loop(){
