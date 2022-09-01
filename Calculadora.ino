6dd88d0de2964332f82bf04cfde84c86fb612075
ThunderGer <Jaames11@hotmail.com>
2022-08-31 23:24:14 -0500

    Prueba para la LCD

--- Calculadora.ino
+++ Calculadora.ino
@@ -1,22 +1,15 @@
-/*        Arduino Mega 2560
+/*
+  LiquidCrystal Library - Custom Characters
 
-Código para la implementación de calculadora
+ Demonstrates how to add custom characters on an LCD  display.
+ The LiquidCrystal library works with all LCD displays that are
+ compatible with the  Hitachi HD44780 driver. There are many of
+ them out there, and you can usually tell them by the 16-pin interface.
 
-Funcionamiento sugerido para el ahorro de pines
-y posterior traslado a un Arduino Pro Micro:
+ This sketch prints "I <heart> Arduino!" and a little dancing man
+ to the LCD.
 
-  1.- Activar o desactivar los teclados matriciales
-      a fin de ahorrar pines y gestionar el control
-      de los mismos (Evitando errores humanos).
-  2.- Compartir bus de datos para las LCD 16x8
-      esto solo puede hacerse gracias al uso
-      especifico de este modelo de display, ya que
-      si se requiere un modelo de 128x64 será
-      necesario contar con 2 puertos ISP para su
-      control.
-
-
-  Pin's for the circuit:
+  The circuit:
  * LCD RS pin to digital pin 1
  * LCD Enable pin to digital pin 0
  * LCD D4 pin to digital pin 5
@@ -28,22 +21,180 @@ y posterior traslado a un Arduino Pro Micro:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3)
  * 10K poterntiometer on pin A0
+
+ created 21 Mar 2011
+ by Tom Igoe
+ modified 11 Nov 2013
+ by Scott Fitzgerald
+ modified 7 Nov 2016
+ by Arturo Guadalupi
+
+ Based on Adafruit's example at
+ https://github.com/adafruit/SPI_VFD/blob/master/examples/createChar/createChar.pde
+
+ This example code is in the public domain.
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalCustomCharacter
+
+ Also useful:
+ http://icontexto.com/charactercreator/
+
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
-#include <SoftwareSerial.h>
+#include <Keypad.h>
 
-LiquidCrystal LCD(27, 26, 25, 24, 23, 22);
+const byte FILAS = 4;
+const byte COLUMNAS = 4;
+char keys [FILAS][COLUMNAS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'},  
+};
 
-void setup(){
+byte pinFilas[FILAS] = {6,7,8,9};
+byte pinColumnas[COLUMNAS] = {10,11,12,13};
+
+Keypad teclado = Keypad(makeKeymap(keys), pinFilas, pinColumnas, FILAS, COLUMNAS);
+char Tecla;
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 1, en = 0, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// make some custom characters:
+byte heart[8] = {
+  0b00000,
+  0b11000,
+  0b01100,
+  0b00011,
+  0b00011,
+  0b00011,
+  0b00011,
+  0b00111
+};
+
+byte smiley[8] = {
+  0b00000,
+  0b01100,
+  0b11000,
+  0b00000,
+  0b00110,
+  0b00010,
+  0b00010,
+  0b10001
+};
+
+byte frownie[8] = {
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00011,
+  0b11000,
+  0b10000,
+  0b10000,
+  0b11000
+};
+
+byte armsDown[8] = {
+  0b00000,
+  0b01100,
+  0b10010,
+  0b10000,
+  0b10000,
+  0b10110,
+  0b10010,
+  0b01100
+};
+
+byte armsUp[8] = {
+  0b00000,
+  0b00000,
+  0b10000,
+  0b00011,
+  0b11000,
+  0b10000,
+  0b10000,
+  0b10000
+};
+
+byte letter[8] = {
+  0b00000,
+  0b01100,
+  0b10010,
+  0b10010,
+  0b10010,
+  0b10010,
+  0b10010,
+  0b01100
+};
+
+byte lettera[8] = {
+  0b00000,
+  0b11000,
+  0b01000,
+  0b01000,
+  0b01100,
+  0b01010,
+  0b01010,
+  0b11011
+};
+
+byte letterb[8] = {
+  0b11111,
+  0b01110,
+  0b01110,
+  0b00100,
+  0b00100,
+  0b00000,
+  0b01110,
+  0b01110
+};
+
+void setup() {
+  // initialize LCD and set up the number of columns and rows:
+  lcd.begin(16, 2);
+
+  // create a new character
+  lcd.createChar(0, heart);
+  // create a new character
+  lcd.createChar(1, smiley);
+  // create a new character
+  lcd.createChar(2, frownie);
+  // create a new character
+  lcd.createChar(3, armsDown);
+  // create a new character
+  lcd.createChar(4, armsUp);
+  // create a new character
+  lcd.createChar(5, letter);
+  // create a new character
+  lcd.createChar(6, lettera);
+  // create a new character
+  lcd.createChar(7, letterb);
+
+  // set the cursor to the top left
+  lcd.setCursor(0, 0);
+
+  // Print a message to the lcd.
+  //lcd.print("I ");
+  lcd.write(byte(0)); // when calling lcd.write() '0' must be cast as a byte
+  //lcd.print(" Arduino! ");
+  lcd.write((byte)1);
+  lcd.write((byte)2);
+  lcd.write((byte)3);
+  lcd.write((byte)4);
+  lcd.write((byte)5);
+  lcd.write((byte)6);
+  lcd.write((byte)7);
   Serial.begin(9600);
-  LCD.begin(16, 2);
 }
 
-void loop(){
-  LCD.setCursor(0, 0);
-  LCD.print("Hola mundo :v");
-  LCD.setCursor(0, 1);
-  LCD.cursor();
+void loop() {  
+  Tecla = teclado.getKey();
+  lcd.setCursor(0, 1);
+  if(Tecla){
+    Serial.println(Tecla);
+  }
 }
