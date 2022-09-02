31b0099fce65f909244b286bc628fda2d784555a
ThunderGer <jaames11@hotmail.com>
2022-09-02 11:01:05 -0500

    add libMagic

--- Calculadora/Calculadora.ino
+++ Calculadora/Calculadora.ino
@@ -35,6 +35,7 @@ y posterior traslado a un Arduino Pro Micro:
 #include <SoftwareSerial.h>;
 #include "libYugioh.h"
 #include "libPokemon.h"
+#include "libMagic.h"
 
 const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
